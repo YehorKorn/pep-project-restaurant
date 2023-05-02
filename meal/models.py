cf1b9019f3fcf5931cb81a5da00239948c619034
Yehor Kornev <dmekkindom00@gmail.com>
2023-05-02 22:00:09 +0300

    Change Meta options on meal. Add field slug to meal.

--- meal/models.py
+++ meal/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Meal(models.Model):
@@ -8,3 +9,19 @@ class Meal(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     preparation_time = models.IntegerField()
     image = models.ImageField(upload_to="meal/")
+    slug = models.SlugField(null=True, blank=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        if not self.slug and self.name:
+            self.slug = slugify(self.name)
+        super(Meal, self).save(
+            force_insert=False, force_update=False, using=None, update_fields=None
+        )
+
+    def __str__(self):
+        return self.name
