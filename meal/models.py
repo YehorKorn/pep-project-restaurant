280f73cc02473f75a92802e2c129817db72a740b
Yehor Kornev <dmekkindom00@gmail.com>
2023-05-02 20:16:03 +0300

    Create model with name Meal and run migrate.

--- meal/models.py
+++ meal/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Meal(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    people = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    preparation_time = models.IntegerField()
+    image = models.ImageField(upload_to="meal/")
