13fae22e36c5e1d95a5578408944d614a1549858
Yehor Kornev <dmekkindom00@gmail.com>
2023-05-02 22:03:15 +0300

    Register Meal in admin

--- meal/admin.py
+++ meal/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from meal.models import Meal
+
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    list_filter = ["price", "people", "preparation_time"]
