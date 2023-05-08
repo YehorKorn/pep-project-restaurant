29a633f1ca4e40c9bfee0ef8243be7de22842185
Yehor Kornev <dmekkindom00@gmail.com>
2023-05-08 14:18:21 +0300

    Add new models in models.py such as Category and Cook

--- meal/models.py
+++ meal/models.py
@@ -2,12 +2,24 @@ from django.db import models
 from django.utils.text import slugify
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "categories"
+
+
 class Meal(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
     people = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     preparation_time = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="meals")
     image = models.ImageField(upload_to="meal/")
     slug = models.SlugField(null=True, blank=True)
 
@@ -25,3 +37,16 @@ class Meal(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Cook(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    about = models.TextField(max_length=150)
+    position = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="cooker/")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.position})"
+
+
