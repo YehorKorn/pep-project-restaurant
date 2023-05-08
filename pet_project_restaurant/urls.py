5d4a67b51359112cc3c29e8a4ede18af18a7b740
Yehor Kornev <dmekkindom00@gmail.com>
2023-05-08 14:14:54 +0300

    Change path for app meal from 'meal/' to ''

--- pet_project_restaurant/urls.py
+++ pet_project_restaurant/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("meal/", include("meal.urls", namespace="meal")),
+    path("", include("meal.urls", namespace="meal")),
 ] + (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
      static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
 
