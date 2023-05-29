3b0a63cba8970da4c91c21f946d74c177821f826
Yehor Kornev <dmekkindom00@gmail.com>
2023-05-29 15:16:38 +0300

    Add decorator 'login_required()' for booking's view

--- meal/views.py
+++ meal/views.py
@@ -1,17 +1,31 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views import generic
 
-from meal.models import Meal
+from meal.models import Meal, Cook
 
 
 class MealListView(generic.ListView):
     model = Meal
     template_name = "menu.html"
     context_object_name = "meal_list"
+    queryset = Meal.objects.select_related("category")
 
 
 class MealDetailView(generic.DetailView):
     model = Meal
+    template_name = "meal/meal_detail.html"
+
+
+class CookListView(generic.ListView):
+    model = Cook
+    template_name = "team.html"
+    context_object_name = "cook_list"
+
+
+# def team(request):
+#     """View function for the home page of the site."""
+#     return render(request, "team.html")
 
 
 def index(request):
@@ -20,30 +34,26 @@ def index(request):
 
 
 def about(request):
-    """View function for the home page of the site."""
+    """View function for the about page of the site."""
     return render(request, "about.html")
 
 
 def menu(request):
-    """View function for the home page of the site."""
+    """View function for the menu page of the site."""
     return render(request, "menu.html")
 
 
+@login_required()
 def booking(request):
-    """View function for the home page of the site."""
+    """View function for booking."""
     return render(request, "booking.html")
 
 
-def team(request):
-    """View function for the home page of the site."""
-    return render(request, "team.html")
-
-
 def contact(request):
-    """View function for the home page of the site."""
+    """View function for the contact page of the site."""
     return render(request, "contact.html")
 
 
 def service(request):
-    """View function for the home page of the site."""
+    """View function for the service page of the site."""
     return render(request, "service.html")
