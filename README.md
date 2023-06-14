8461196fba86f887c036e4a9abeb6df9dbd6d288
Yehor Kornev <dmekkindom00@gmail.com>
2023-06-14 11:04:43 +0300

    Add fixture in Readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pet-project-restaurant
+
+python manage.py loaddata pet_project_restaurant_db_data.json
--- meal/urls.py
+++ meal/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from meal.views import (
     MealListView,
     MealDetailView,
+    MealUpdateView,
     index,
     about,
     booking,
@@ -18,6 +19,7 @@ urlpatterns = [
     path("contact/", contact, name="contact"),
     path("menu/", MealListView.as_view(), name="menu"),
     path("menu/<slug:slug>/", MealDetailView.as_view(), name="meal-detail"),
+    path("menu/update/<slug:slug>/", MealUpdateView.as_view(), name="meal-update"),
 ]
 
 app_name = "meal"
